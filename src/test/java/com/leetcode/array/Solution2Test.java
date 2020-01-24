d32dec20a9f8b4479c0bae2f79a8c960496bc4fe
Шипов Евгений Викторович <shipovev@alfastrah.ru>
2020-01-24 16:12:59 +0300

    сделал задачу - Best Time to Buy and Sell Stock II

---
+++ src/test/java/com/leetcode/array/Solution2Test.java
@@ -0,0 +1,28 @@
+package com.leetcode.array;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution2Test {
+
+    private Solution2 solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution2();
+    }
+
+    @Test
+    void maxProfit() {
+        int result1 = solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+        assertEquals(7, result1);
+
+        int result2 = solution.maxProfit(new int[]{1,2,3,4,5});
+        assertEquals(4, result2);
+
+        int result3 = solution.maxProfit(new int[]{7,6,4,3,1});
+        assertEquals(0, result3);
+    }
+}
